7b3eb1f2c786c1cc649b492aee090c5d41d35483
carogomezt <carolina9511@gmail.com>
2017-06-09 11:52:44 -0500

    modifing word view

--- controllers/quotation.js
+++ controllers/quotation.js
@@ -669,23 +669,23 @@ exports.search = (req, res) => {
 };
 
 exports.exportToWord = (req, res) => {
-  // res.render("quotation/word");
-  ejs.renderFile("views/quotation/word.ejs", (err, html) => {
-    const options = {
-      orientation: "landscape",
-      margins: {
-        top: "3cm",
-        right: "2.09cm",
-        bottom: "2.5cm",
-        left: "1.75cm"
-      }
-    };
-    const docx = HtmlDocx.asBlob(html, options);
-    fs.writeFile(`${__dirname}/../public/docs/cotizacion.docx`, docx, err => {
-      if (err) console.log(err);
-      res.redirect("/docs/cotizacion.docx");
-    });
-  });
+  res.render("quotation/word");
+  // ejs.renderFile("views/quotation/word.ejs", (err, html) => {
+  //   const options = {
+  //     orientation: "portrait",
+  //     margins: {
+  //       top: "3cm",
+  //       right: "2.09cm",
+  //       bottom: "2.5cm",
+  //       left: "1.75cm"
+  //     }
+  //   };
+  //   const docx = HtmlDocx.asBlob(html, options);
+  //   fs.writeFile(`${__dirname}/../public/docs/cotizacion.docx`, docx, err => {
+  //     if (err) console.log(err);
+  //     res.redirect("/docs/cotizacion.docx");
+  //   });
+  // });
 };
 
 exports.menu = (req, res) => {

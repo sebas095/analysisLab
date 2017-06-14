48f524db3743fd5f80fa239e4ff3e98d8d2bd60c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-06-14 10:57:27 -0500

    fix parameter modal

--- public/js/samplesManager.js
+++ public/js/samplesManager.js
@@ -1,5 +1,5 @@
-let count = 0;
-let rowSpan = 1;
+let count = 0, count2 = 0;
+let rowSpan = 1, rowSpan2 = 1;
 jQuery(document).ready($ => {
   $("#sampleForm").submit(ev => {
     ev.preventDefault();
@@ -133,90 +133,110 @@ jQuery(document).ready($ => {
       $("#totalPrice").text(total);
     });
 
-    $(".numberSamples").on("keyup mouseup", function() {
-      if (this.value) {
-        const price = Number($(this).parent().prev().text());
-        $(this).parent().next().text(this.value * price);
-        let total = 0;
-        const $numberSamples = $(".numberSamples");
-        for (let i = 0; i < $numberSamples.length; i++) {
-          total += Number($($numberSamples[i]).parent().next().text());
-        }
-        $("#totalPrice").text(total);
-      } else {
-        setTimeout(
-          () => {
-            if (!this.value) {
-              $(this).val(0);
-              $(this).parent().next().text("0");
-              let total = 0;
-              const $numberSamples = $(".numberSamples");
-              for (let i = 0; i < $numberSamples.length; i++) {
-                total += Number($($numberSamples[i]).parent().next().text());
-              }
-              $("#totalPrice").text(total);
-            }
-          },
-          1000
-        );
-      }
-    });
-
+    updateInputs();
     return false;
   });
 
   $("#parameterForm").submit(ev => {
     ev.preventDefault();
-    // console.log("HOLA");
-    // const input = document.getElementsByClassName("parameterInput");
-    // let sampleData = "";
-    // const parent = $("#otherParameter").parent().parent();
-    // const row = getTagAndRowSpan(parent);
-    // $("#otherParameter").remove();
-    //
-    // for (let i = 0; i < input.length; i++) {
-    //   if ((i + 1) % 3 === 0) {
-    //     sampleData += "<span></span>";
-    //     if (i === input.length - 3) {
-    //       sampleData += `
-    //         <td>
-    //           <a href="#" id="otherParameter" class="newParameter" data-toggle="modal" data-target="#parameterModal">
-    //             <i class="fa fa-plus icon-position" aria-hidden="true"></i>
-    //           </a>
-    //           <a href="#" class="deleteParameter">
-    //             <i class="fa fa-minus-circle icon-red icon-position" aria-hidden="true"></i>
-    //           </a>
-    //           ${input[i].value}
-    //         </td>`;
-    //     } else {
-    //       sampleData += `
-    //         <td>
-    //           <a href="#" class="deleteParameter">
-    //             <i class="fa fa-minus-circle icon-red icon-position" aria-hidden="true"></i>
-    //           </a>
-    //           ${input[i].value}
-    //         </td>`;
-    //     }
-    //   } else {
-    //     sampleData += `<td>${input[i].value}</td>`;
-    //   }
-    //
-    //   if (i % 3 === 0 && i > 0) {
-    //     sampleData += `
-    //       <td>
-    //         <input type="number" style="text-align:center" min="0" value="0" class="numberSamples" name="numberSamples">
-    //       </td>
-    //       <td>0</td>`;
-    //   }
-    // }
-    //
-    // sampleData = sampleData.replace(/<span><\/span>/g, "</tr><tr>");
-    // $(sampleData).insertAfter(parent);
-    // row.tag.children().first().attr("rowspan", data.rowSpan + 1);
-    // $(".modal").modal("hide");
-    // $(".modal").css("display", "none");
-    // document.getElementById("parameterForm").reset();
+    const input = document.getElementsByClassName("parameterInput");
+    let sampleData = "";
+    const parent = $("#otherParameter").parent().parent();
+    const row = getTagAndRowSpan(parent);
+    $("#otherParameter").remove();
 
+    for (let i = 0; i < input.length; i++) {
+      if (i % 3 === 0) {
+        sampleData += "<span></span>";
+        if (i === input.length - 3) {
+          sampleData += `
+            <td>
+              <a href="#" id="otherParameter" class="newParameter" data-toggle="modal" data-target="#parameterModal">
+                <i class="fa fa-plus icon-position" aria-hidden="true"></i>
+              </a>
+              <a href="#" class="deleteOtherParameter">
+                <i class="fa fa-minus-circle icon-red icon-position" aria-hidden="true"></i>
+              </a>
+              ${input[i].value}
+            </td>`;
+        } else {
+          sampleData += `
+            <td>
+              <a href="#" class="deleteOtherParameter">
+                <i class="fa fa-minus-circle icon-red icon-position" aria-hidden="true"></i>
+              </a>
+              ${input[i].value}
+            </td>`;
+        }
+      } else {
+        sampleData += `<td>${input[i].value}</td>`;
+      }
+
+      if ((i + 1) % 3 === 0 && i > 0) {
+        sampleData += `
+          <td>
+            <input type="number" style="text-align:center" min="0" value="0" class="numberSamples" name="numberSamples">
+          </td>
+          <td>0</td>`;
+      }
+    }
+
+    sampleData = sampleData.replace(/<span><\/span>/g, "</tr><tr>");
+    $(sampleData).insertAfter(parent);
+    row.tag.children().first().attr("rowspan", row.rowSpan + rowSpan2);
+    $(".modal").modal("hide");
+    $(".modal").css("display", "none");
+    document.getElementById("parameterForm").reset();
+
+    count2 = 0;
+    rowSpan2 = 1;
+
+    if ($(".parameterInput").length > 3) {
+      const sampleInput = $(".parameterInput");
+      for (let i = 3; i < sampleInput.length; i += 3) {
+        $(sampleInput[i]).parent().parent().parent().parent().remove();
+      }
+    }
+
+    $(".deleteOtherParameter").click(function(ev) {
+      ev.preventDefault();
+      const row = $(this).parent().parent();
+      const data = getTagAndRowSpan(row);
+      if ($(this).prev().attr("class") === "newParameter") {
+        if (data.rowSpan > 1) {
+          const length = row.prev().children().length;
+          const prev = $(row.prev().children()[length - 5]);
+          prev.prepend(
+            `<a href="#" id="otherParameter" class="newParameter" data-toggle="modal" data-target="#parameterModal">
+              <i class="fa fa-plus icon-position" aria-hidden="true"></i>
+            </a>`
+          );
+          row.remove();
+          data.tag.children().first().attr("rowspan", data.rowSpan - 1);
+        } else {
+          data.tag.remove();
+        }
+      } else {
+        if ($(this).parent().prev().attr("rowspan")) {
+          const td = $(this).parent().prev();
+          td.attr("rowspan", data.rowSpan - 1);
+          row.next().prepend(td);
+          row.remove();
+        } else {
+          row.remove();
+          data.tag.children().first().attr("rowspan", data.rowSpan - 1);
+        }
+      }
+
+      let total = 0;
+      const $numberSamples = $(".numberSamples");
+      for (let i = 0; i < $numberSamples.length; i++) {
+        total += Number($($numberSamples[i]).parent().next().text());
+      }
+      $("#totalPrice").text(total);
+    });
+
+    updateInputs();
     return false;
   });
 });
@@ -233,6 +253,37 @@ function getTagAndRowSpan(row) {
       curr = curr.prev();
     }
   }
+}
+
+function updateInputs() {
+  $(".numberSamples").on("keyup mouseup", function() {
+    if (this.value) {
+      const price = Number($(this).parent().prev().text());
+      $(this).parent().next().text(this.value * price);
+      let total = 0;
+      const $numberSamples = $(".numberSamples");
+      for (let i = 0; i < $numberSamples.length; i++) {
+        total += Number($($numberSamples[i]).parent().next().text());
+      }
+      $("#totalPrice").text(total);
+    } else {
+      setTimeout(
+        () => {
+          if (!this.value) {
+            $(this).val(0);
+            $(this).parent().next().text("0");
+            let total = 0;
+            const $numberSamples = $(".numberSamples");
+            for (let i = 0; i < $numberSamples.length; i++) {
+              total += Number($($numberSamples[i]).parent().next().text());
+            }
+            $("#totalPrice").text(total);
+          }
+        },
+        1000
+      );
+    }
+  });
 }
 
 function addParameter(id) {
@@ -253,7 +304,7 @@ function addParameter(id) {
         </div>
         <div class="col-sm-3">
           <div class="form-group">
-            <input type="text" class="form-control sampleInput" required="">
+            <input type="number" min="0" class="form-control sampleInput" required="">
           </div>
         </div>
         <div class="col-sm-2 icons-parameters">
@@ -290,6 +341,72 @@ function removeParameter(id) {
         const prev = parent.prev().children().children().last();
         prev.prepend(
           `<span class="addParameter" onclick="addParameter('#add${count}')" id="add${count}">
+            <i class="fa fa-plus fa-2x fix-position" aria-hidden="true"></i>
+          </span>`
+        );
+        parent.remove();
+      } else {
+        parent.remove();
+      }
+    }
+  }
+}
+
+function addOtherParameter(id) {
+  rowSpan2++, count2++;
+  $(id).remove();
+  $("#othersParameters").append(
+    `<div class="row">
+      <div class="col-sm-12">
+        <div class="col-sm-3">
+          <div class="form-group">
+            <input type="text" class="form-control parameterInput" required="">
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="form-group">
+            <input type="text" class="form-control parameterInput" required="">
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <div class="form-group">
+            <input type="number" min="0" class="form-control parameterInput" required="">
+          </div>
+        </div>
+        <div class="col-sm-2 icons-parameters">
+          <span class="addParameter" onclick="addOtherParameter('#adds${count2}')" id="adds${count2}"><i class="fa fa-plus fa-2x fix-position" aria-hidden="true"></i></span>
+          <span class="removeParameter" onclick="removeOtherParameter('#removes${count2}')" id="removes${count2}"><i class="fa fa-minus-circle fa-2x fix-position icon-red" aria-hidden="true"></i></span>
+        </div>
+      </div>
+    </div>`
+  );
+}
+
+function removeOtherParameter(id) {
+  if ($(".parameterInput").length > 3) {
+    rowSpan2--;
+    const parent = $(id).parent().parent().parent();
+    const next = parent.next();
+    const center = parent.children().children().children().children().first();
+
+    if (center.attr("class") === "center") {
+      parent.remove();
+      const div = next.children().children().last();
+      div.removeClass("icons-parameters");
+      const parameterInput = $(".parameterInput");
+      const labels = ["Parámetro", "Método", "Precio"];
+      for (let i = 0; i < 3; i++) {
+        $(
+          `<div class="center">
+            <label>${labels[i]}</label>
+          </div>`
+        ).insertBefore($(parameterInput[i]));
+      }
+    } else {
+      if ($(id).prev().attr("class") === "addParameter") {
+        const prev = parent.prev().children().children().last();
+        prev.prepend(
+          `<span class="addParameter" onclick="addOtherParameter('#adds${count2}')" id="adds${count2}">
             <i class="fa fa-plus fa-2x fix-position" aria-hidden="true"></i>
           </span>`
         );

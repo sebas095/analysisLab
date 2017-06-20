15feeb5b25bdd63b1706ce6e4c9cf8f2a37e3465
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-06-19 20:58:50 -0500

    update config

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     url: "mongodb://127.0.0.1:27017/analysis-lab"
   },
   secret: "c8f5f276-02b9-451c-833f-6ea3da52b343",
-  url: "http://localhost:3000"
+  url: "http://sirius.utp.edu.co/labaguasyalimentos"
 };

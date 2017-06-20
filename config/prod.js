63662adf8439d7f73530a9c72241969ce507465e
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-06-20 15:38:17 -0500

    update config

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,10 @@
 module.exports = {
   db: {
-    url: "mongodb://127.0.0.1:27017/analysis-lab"
+    url: "mongodb://" +
+      process.env.MONGODB_PORT_27017_TCP_ADDR +
+      ":" +
+      process.env.MONGODB_PORT_27017_TCP_PORT +
+      "/websirius-dev"
   },
   secret: "c8f5f276-02b9-451c-833f-6ea3da52b343",
   url: "http://sirius.utp.edu.co/labaguasyalimentos"
